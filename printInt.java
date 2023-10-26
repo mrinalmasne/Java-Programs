90bd3c03bd9598a871f62b99837d8b698f6c16ec
Mrinal Masne <76609436+mrinalmasne@users.noreply.github.com>
2023-10-26 14:32:53 +0530

    Add files via upload

---
+++ printInt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class printInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter no:");
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		System.out.println("Number:"+n);
+
+	}
+
+}
+
